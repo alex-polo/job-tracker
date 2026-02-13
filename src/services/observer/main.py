9ec45808509822ba9758584ac72be8149ff00fdf
Apolo <polonnikov.alexander@gmail.com>
2026-02-13 12:00:03 +0300

    feat: add main observer

--- src/services/observer/main.py
+++ src/services/observer/main.py
@@ -2,16 +2,31 @@ import asyncio
 import logging
 from typing import TYPE_CHECKING
 
+from src.core.conf.classes import SourceType
+from src.core.database import DB_MANAGER
+
+from .loaders.head_hunter_loader import HeadHunterLoader
+from .repositories.vacancy import VacancyRepository
 from .scheduler import ParseScheduler
-from .tasks import hh_parse_task
+from .tasks.polling_task import PollingTask
 
 if TYPE_CHECKING:
     from src.core.conf import ObserverSettings
 
+    from .tasks import ISchedulerTask
+
 log = logging.getLogger(__name__)
 
 
-async def _main(settings: ObserverSettings) -> None:
+def make_polling_task() -> ISchedulerTask:
+    """Create polling ISchedulerTask instance."""
+    return PollingTask(
+        loader=HeadHunterLoader(),
+        repository=VacancyRepository(),
+    )
+
+
+async def main(settings: ObserverSettings) -> None:
     """Main function."""
     scheduler = ParseScheduler(settings=settings.scheduler)
 
@@ -21,26 +36,30 @@ async def _main(settings: ObserverSettings) -> None:
             source.source_type,
             source.url,
         )
-
-        scheduler.add_job(
-            job_id=source.source_type,
-            func=hh_parse_task,
-            interval_minutes=source.period_minutes,
-            task_args=(source.url,),
-        )
-        log.debug("Added job for source: %s", source.source_type)
+        if source.source_type == SourceType.HH:
+            scheduler.add_job(
+                job_id=str(source.source_type),
+                func=make_polling_task().run,
+                interval_minutes=source.period_minutes,
+                task_args=(source.url,),
+            )
+            log.debug("Added job for source: %s", source.source_type)
 
     scheduler.start()
 
     try:
         await asyncio.Event().wait()
     finally:
+        log.info("Shutting down scheduler")
         scheduler.shutdown()
+        log.info("Disposing database engine")
+        await DB_MANAGER.dispose_engine()
+        log.info("Exiting")
         log.info("Observer stopped")
 
 
 def run_observer(settings: ObserverSettings) -> None:
     """Run observer."""
     log.info("Start observer")
-    asyncio.run(_main(settings=settings))
+    asyncio.run(main(settings=settings))
     log.info("Stop observer")
