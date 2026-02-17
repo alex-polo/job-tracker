c6887c726e811eb41772aea7e7d4dbc9de51da96
Apolo <polonnikov.alexander@gmail.com>
2026-02-17 18:13:53 +0300

    feat(tasks): add MQPublisher to PollingTask for sending vacancies

--- src/services/observer/tasks/base_task.py
+++ src/services/observer/tasks/base_task.py
@@ -1,18 +1,27 @@
+from __future__ import annotations
+
 from abc import ABC, abstractmethod
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from src.services.observer.loaders import ILoader
+    from src.services.observer.messaging.rabbitmq import MQPublisher
     from src.services.observer.repositories import IRepository
 
 
 class ISchedulerTask(ABC):
     """Interface for scheduler tasks."""
 
-    def __init__(self, loader: ILoader, repository: IRepository) -> None:
+    def __init__(
+        self,
+        loader: ILoader,
+        repository: IRepository,
+        mq_publisher: MQPublisher,
+    ) -> None:
         """Initialize task."""
         self._loader = loader
         self._repository = repository
+        self.mq_publisher: MQPublisher = mq_publisher
 
     @abstractmethod
     async def run(self) -> None:
--- src/services/observer/tasks/polling_task.py
+++ src/services/observer/tasks/polling_task.py
@@ -12,7 +12,12 @@ class PollingTask(ISchedulerTask):
         """Run task."""
         for vacancy in await self._loader.load(url=url):
             if not await self._repository.exists(vacancy_hash=vacancy.hash):
-                await self._repository.save(vacancy_hash=vacancy.hash)
+                if await self.mq_publisher.send_message(vacancy=vacancy):
+                    await self._repository.save(vacancy_hash=vacancy.hash)
+                else:
+                    log.error(
+                        "Failed to send vacancy to RabbitMQ: %s", vacancy
+                    )
                 log.debug("Vacancy saved: %s", vacancy.hash)
             else:
                 log.debug("Vacancy already exists: %s", vacancy.hash)
